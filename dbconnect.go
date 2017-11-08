3dfc8668a7f2fccfe681e26e26ab2dcb3597941f
AllGoodPlanes <hello_to_ric@hotmail.com>
2017-11-08 10:54:32 +0000

    with passcodes

--- dbconnect.go
+++ dbconnect.go
@@ -9,11 +9,11 @@ import (
 )
 
 const (
-	MongoDBHosts = "xxxxxxxxx"
-	AuthDatabase = "xxxxxxxxxxxx"
-	AuthUserName = "xxxxxxxx"
-	AuthPassword = "xxxxxxxxxxx"
-	TestDatabase = "xxxxxxxxx"
+	MongoDBHosts = "ds017678.mlab.com:17678"
+	AuthDatabase = "heroku_549kcv64"
+	AuthUserName = "heroku_549kcv64"
+	AuthPassword = "s4viie11a5su1f0uj20r38i3qq"
+	TestDatabase = "heroku_549kcv64"
 )
 
 var db *sql.DB
@@ -21,7 +21,7 @@ var mongoSession *mgo.Session
 
 func init() {
 	var err error
-	db, err = sql.Open("postgres", "postgres://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+	db, err = sql.Open("postgres", "postgres://gzyvmlrlbuhxni:CFMYUehQv56yBGEWHdxy3psZ3T@ec2-54-195-241-96.eu-west-1.compute.amazonaws.com:5432/dcb0vrh3du4a7e")
 	if err != nil {
 		log.Fatal(err)
 	}
